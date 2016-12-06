4e9bf2077775efb3f921097bf22baf70d39eef5d
bezmolwniy <bezmolwniy@mail.ru>
2016-12-06 17:49:02 +0900

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/about', to: 'static_pages#about'
   devise_for :users
   resources :posts #для каждого действия в контроллеере свой урл
   root to: 'posts#index'

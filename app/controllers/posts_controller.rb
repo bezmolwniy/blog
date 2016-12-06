f6c5d23366cf54c069a400f382a1507f92d85795
bezmolwniy <bezmolwniy@mail.ru>
2016-12-06 17:52:16 +0900

    Update posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action  :authenticate_user!, except: [:index, :show]
 
   def index
-    @posts = Post.order("created_at DESC")
+    @posts = Post.order("CREATED_AT DESC")
   end
-
-  def show
-  end
+  
+  #params переменная в которой все данные которые мы ввели
+  def show; end
 
   def new
     @post = Post.new
@@ -16,35 +16,39 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to @post, notice: "Post was successfully created!"
+      redirect_to @post #редирект перенаправляет на страницу
     else
-      render "new"
+      render "new" #render use for action на действие
     end
   end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-    if @post.update(post_params)
-      redirect_to @post, notice: "Post was updated"
+  def update # не гет запрос(патч)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)#
+      redirect_to @post
     else
       render "edit"
     end
   end
 
   def destroy
+    @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_url, notice: "Post was deleted"
+    redirect_to posts_path
   end
 
   private
-
+  
   def post_params
-    params.require(:post).permit(:title, :body) #нужно для сеттеров и геттеров
+    params.require(:post).permit(:title, :body)
   end
 
   def find_post
     @post = Post.find(params[:id])
   end
 end
+
+#:post simbolpost(type of date)
+#

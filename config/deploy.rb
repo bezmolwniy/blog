a3511fb9fbb882802153e329d062ba85f6f2b52c
bezmolwniy <bezmolwniy@mail.ru>
2016-12-13 20:07:25 +0900

    Update mina receipt

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
+require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
-require 'mina/rvm'
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
@@ -8,83 +8,107 @@ require 'mina/rvm'
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
 
-set :domain, '78.155.207.47'
-set :user, 'blog'
-set :deploy_to, "/home/#{fetch(:user)}/app"
+set :domain, '77.244.214.151'
+set :user, 'rails-demo'
+set :deploy_to, "/home/#{user}/app"
 set :repository, 'git@github.com:bezmolwniy/blog.git'
 set :branch, 'master'
-set :rvm_use_path, '/etc/profile.d/rvm.sh'
+
+# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
+# They will be linked in the 'deploy:link_shared_paths' step.
+set :shared_paths, ['config/database.yml', 'config/secrets.yml', 'log', 'public/system']
 
 # Optional settings:
-#   set :user, 'foobar'          # Username in the server to SSH to.
-#   set :port, '30000'           # SSH port number.
+#   set :user, 'foobar'    # Username in the server to SSH to.
+#   set :port, '30000'     # SSH port number.
 #   set :forward_agent, true     # SSH forward_agent.
 
-# They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# This task is the environment that is loaded all remote run commands, such as
+# This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
 task :environment do
   ruby_version = File.read('.ruby-version').strip
   raise "Couldn't determine Ruby version: Do you have a file .ruby-version in your project root?" if ruby_version.empty?
-
-  invoke :'rvm:use', ruby_version
+  queue %{
+    source /etc/profile.d/rvm.sh
+    rvm use #{ruby_version} || exit 1
+  }
 end
 
-task :setup do
+task :setup => :environment do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/log"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/log"]
 
-  in_path(fetch(:shared_path)) do
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/config"]
 
-    command %[mkdir config]
+  # Add the repository server to .ssh/known_hosts
+  if repository
+    repo_host = repository.split(%r{@|://}).last.split(%r{:|\/}).first
+    repo_port = /:([0-9]+)/.match(repository) && /:([0-9]+)/.match(repository)[1] || '22'
 
-    # Create database.yml for Postgres if it doesn't exist
-    path_database_yml = "config/database.yml"
-    database_yml = %[production:
-  database: #{fetch(:user)}
+    queue %[
+      if ! ssh-keygen -H  -F #{repo_host} &>/dev/null; then
+        ssh-keyscan -t rsa -p #{repo_port} -H #{repo_host} >> ~/.ssh/known_hosts
+      fi
+    ]
+  end
+
+  # Create database.yml for Postgres if it doesn't exist
+  path_database_yml = "#{deploy_to}/#{shared_path}/config/database.yml"
+  database_yml = %[production:
+  database: #{user}
   adapter: postgresql
   pool: 5
   timeout: 5000]
-    command %[test -e #{path_database_yml} || echo "#{database_yml}" > #{path_database_yml}]
+  queue! %[ test -e #{path_database_yml} || echo "#{database_yml}" > #{path_database_yml} ]
 
-    # Create secrets.yml if it doesn't exist
-    path_secrets_yml = "config/secrets.yml"
-    secrets_yml = %[production:\n  secret_key_base:\n    #{`rake secret`.strip}]
-    command %[test -e #{path_secrets_yml} || echo "#{secrets_yml}" > #{path_secrets_yml}]
-    
-    # Remove others-permission for config directory
-    command %[chmod -R o-rwx config]
-  end
+  # Create secrets.yml if it doesn't exist
+  path_secrets_yml = "#{deploy_to}/#{shared_path}/config/secrets.yml"
+  secret =
+  secrets_yml = %[production:
+  secret_key_base:
+    #{`rake secret`.strip}]
+  queue! %[ test -e #{path_secrets_yml} || echo "#{secrets_yml}" > #{path_secrets_yml} ]
+
+  queue! %[chmod g+rx,u+rwx,o-rwx "#{deploy_to}/#{shared_path}/config"]
 
 end
 
 desc "Deploys the current version to the server."
-task :deploy do
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  # invoke :'git:ensure_pushed'
+task :deploy => :environment do
+  to :before_hook do
+    # Put things to run locally before ssh
+  end
   deploy do
     # Put things that will set up an empty directory into a fully set-up
     # instance of your project.
     invoke :'git:clone'
+
+    fetch(:shared_dirs, []).each do |linked_dir|
+      command %{mkdir -p #{File.dirname("./#{linked_dir}")}}
+      command %{rm -rf "./#{linked_dir}"}
+      command %{ln -s "#{fetch(:shared_path)}/#{linked_dir}" "./#{linked_dir}"}
+    end
+
     # Workaround for https://github.com/mina-deploy/mina/issues/452
     fetch(:shared_files, []).each do |linked_path|
       command %{rm -f "./#{linked_path}"}
     end
+
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
-    on :launch do
-      # command "sudo service #{fetch(:user)} restart"
+    to :launch do
+      queue "sudo service #{user} restart"
     end
   end
-
-  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
-  # run :local { say 'done' }
 end
 
 # For help in making your deploy script, see the Mina documentation:
 #
-#  - https://github.com/mina-deploy/mina/docs
+#  - http://nadarei.co/mina
+#  - http://nadarei.co/mina/tasks
+#  - http://nadarei.co/mina/settings
+#  - http://nadarei.co/mina/helpers
